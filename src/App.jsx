254cc641ddbfc59902d340ecb006a67f97a925e4
Aaron Russell <cpl.adrusselljr@gmail.com>
2022-03-15 10:30:33 -0400

    modified App.jsx/App.css

--- src/App.jsx
+++ src/App.jsx
@@ -76,26 +76,46 @@ export class App extends Component {
 
                     <h1>Two Truths and a Lie</h1>
 
-                    <label>Username: </label>
-                    <input name="userName" value={ this.state.userName } onChange={ this.changeHandlerUserName }/><br/>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <input className='form-control' name="userName" value={ this.state.userName } onChange={ this.changeHandlerUserName }/><br/>
+                    </div>
 
-                    <label>Prompt 1: </label>
-                    <input type="text" name="promptOne" value={ this.state.promptObj.promptOne.input } onChange={ this.changeHandlerPrompts }/>
-                    <label>  isLie: </label>
-                    <input type="checkbox" name="promptOne" checked={ this.state.promptObj.promptOne.isLie } onChange={ this.changeHandlerCheckbox }/><br/>
+                    <p>----------------------------------------------</p>
 
-                    <label>Prompt 2: </label>
-                    <input type="text" name="promptTwo" value={ this.state.promptObj.promptTwo.input } onChange={ this.changeHandlerPrompts }/>
-                    <label>  isLie: </label>
-                    <input type="checkbox" name="promptTwo" checked={ this.state.promptObj.promptTwo.isLie } onChange={ this.changeHandlerCheckbox }/><br/>
+                    <div className="form-group">
+                        <label>Prompt 1: </label>
+                        <input className='form-control' type="text" name="promptOne" value={ this.state.promptObj.promptOne.input } onChange={ this.changeHandlerPrompts }/>
+                        <label className='form-check-label'>  isLie: </label>
+                        <input className='form-check-input' type="checkbox" name="promptOne" checked={ this.state.promptObj.promptOne.isLie } onChange={ this.changeHandlerCheckbox }/><br/>
+                    </div>
 
-                    <label>Prompt 3: </label>
-                    <input type="text" name="promptThree" value={ this.state.promptObj.promptThree.input } onChange={ this.changeHandlerPrompts }/>
-                    <label>  isLie: </label>
-                    <input type="checkbox" name="promptThree" checked={ this.state.promptObj.promptThree.isLie } onChange={ this.changeHandlerCheckbox }/><br/>
+                    <p>----------------------------------------------</p>
 
-                    <label>Vote: </label>
-                    <input type="number" name="vote" value={ this.state.vote } onChange={ this.chsangeHandlerVote }/><br/>
+                    <div className="form-group">
+                        <label>Prompt 2: </label>
+                        <input className='form-control' type="text" name="promptTwo" value={ this.state.promptObj.promptTwo.input } onChange={ this.changeHandlerPrompts }/>
+                        <label className='form-check-label'>  isLie: </label>
+                        <input className='form-check-input' type="checkbox" name="promptTwo" checked={ this.state.promptObj.promptTwo.isLie } onChange={ this.changeHandlerCheckbox }/><br/>
+                    </div>
+
+                    <p>----------------------------------------------</p>
+
+                    <div className="form-group">
+                        <label>Prompt 3: </label>
+                        <input className='form-control' type="text" name="promptThree" value={ this.state.promptObj.promptThree.input } onChange={ this.changeHandlerPrompts }/>
+                        <label className='form-check-label'>  isLie: </label>
+                        <input className='form-check-input' type="checkbox" name="promptThree" checked={ this.state.promptObj.promptThree.isLie } onChange={ this.changeHandlerCheckbox }/><br/>
+                    </div>
+
+                    <p>----------------------------------------------</p>
+
+                    <div className="form-group">
+                        <label>Vote: </label>
+                        <input className='form-control' type="number" name="vote" value={ this.state.vote } onChange={ this.chsangeHandlerVote }/><br/>
+                    </div>
+
+                    <p>----------------------------------------------</p>
 
                     <button className="btn btn-primary">Send Prompt</button>
                     <button className="btn btn-primary">Send Vote</button>
